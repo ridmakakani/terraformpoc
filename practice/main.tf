b18fea8acca0d65b33f0297343204fa0a92fdfcc
ridmakakani <69663027+ridmakakani@users.noreply.github.com>
2021-01-27 16:13:55 +0530

    Update main.tf

--- practice/main.tf
+++ practice/main.tf
@@ -5,12 +5,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "info_test_rg"
+  name     = "info_test_rg1"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "infostorageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
